695a053956712c5c442cd01008c43ecb0ab3bca2
Guilherme Henrique dos Santos <dos_santos_98@hotmail.com>
2020-02-06 19:29:32 -0300

    Added some comments

--- DoubleDeepQNetwork/Utils.py
+++ DoubleDeepQNetwork/Utils.py
@@ -14,19 +14,24 @@ class WeightSaver:
 
     def __call__(self, model, step):
 
+        # Template for weight file name
         template = "weights_ep_{}.h5"
 
         if step % self._save_freq == 0:
 
+            # Searches for already existing weight files
             weight_files = glob.glob(self._weights_dir + '*.h5')
 
             if weight_files:
 
+                # Sorts the list of weight files by last modification date (older to newer)
                 weight_files.sort(key=os.path.getmtime, reverse=False)
 
+                # If the maximum of weight files was reached then delete the older one
                 if len(weight_files) == self._max_save:
                     os.remove(weight_files[0])
 
+                # Gets the number of newest weight file
                 last_saved = int(re.split("[./_]", weight_files[-1])[-2])
 
                 file_name = self._weights_dir + template.format(last_saved + self._save_freq)
@@ -38,14 +43,18 @@ class WeightSaver:
 
 def restore_last_saved(model, agent, weights_dir):
 
+    # Searches for already existing weight files
     weight_files = glob.glob(weights_dir + '*.h5')
     if weight_files:
+
+        # Sorts the list of weight files by last modification date (newer to older)
         weight_files.sort(key=os.path.getmtime, reverse=True)
         model.restore_weights(agent, weight_files[0])
         print(f"Restored weights from file: {weight_files[0]}")
 
 
 def preprocess_frames(frames, new_size=[84, 84]):
+    """ Crop unnecessary pixels, resize image and apply dimensionality redution to save memory"""
 
     channels = len(frames)
     height, width = new_size
@@ -64,22 +73,22 @@ def preprocess_frames(frames, new_size=[84, 84]):
 
 
 def dimensionality_reduction(image, threshold=0.8):
+    """ Dimensionality reduction using Singular Value Decomposition (SVD) """
 
     # If image is not a black screen (all elements are not zero)
     if image.all():
-        """ Performs singular value decomposition """
+
+        # Performs singular value decomposition
         u, s, vh = np.linalg.svd(image, full_matrices=False)
 
-        """ Calculates cumulative energy of the singular values.
-        """
+        # Calculates cumulative energy of the singular values.
         cumsum = np.cumsum(s) / np.sum(s)
 
-        """ Gets the number of singular values that have a cumulative 
-            energy at least equal or above the specified threshold. 
-        """
+        # Gets the number of singular values that have a cumulative
+        # energy at least equal or above the specified threshold
         quantity = np.where(cumsum >= threshold)[0][0]
 
-        """ Return the reconstructed image in a compressed form """
+        # Return the reconstructed image in a compressed form
         return np.dot(np.multiply(u[:, :quantity], s[:quantity]), vh[:quantity, :])
     else:
         return image
